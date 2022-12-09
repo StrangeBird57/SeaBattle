c83415ddc09272366bf2f7c67cc97936ec380dce
Vladislav Zhukov <102047649+StrangeBird57@users.noreply.github.com>
2022-12-09 15:50:08 +0300

    add set_border method

--- SeaBattle/Source.cpp
+++ SeaBattle/Source.cpp
@@ -81,14 +81,16 @@ void set_cursor(ll x, ll y) {
 
 class Ship {
 private:
-	ll size, hp;
+	ll size, hp, x, y;
 	bool is_alive, orientation; //0-vertical 1-gorizontal
 public:
-	Ship(ll new_size, bool new_orientation);
+	Ship(ll new_size, ll new_x, ll new_y, bool new_orientation);
 	void hit();
 	bool get_is_alive();
 	bool get_orientation();
 	ll get_size();
+	ll get_x();
+	ll get_y();
 };
 
 class Field {
@@ -102,18 +104,22 @@ private:
 	void show_border(ll x, ll y);
 	void show_field(ll x, ll y);
 public:
-	bool check_pos(ll x, ll y, Ship ship);
-	void set_ship(ll x, ll y, Ship ship);
+	bool check_pos(Ship ship);
+	void set_ship(Ship ship);
 	void generate_field();
 	Field(Color new_border_color);
 	void show(ll x, ll y);
-	void update_field(ll x, ll y);
+	void set_border(Ship ship);
+	bool check_pos(ll x, ll y);
+	ll update_field(ll x, ll y);
 };
 
-Ship::Ship(ll new_size = 1, bool new_orientation = 1) {
+Ship::Ship(ll new_size, ll new_x, ll new_y, bool new_orientation) {
 	size = new_size;
 	hp = size;
 	is_alive = 1;
+	x = new_x;
+	y = new_y;
 	orientation = new_orientation;
 }
 
@@ -132,6 +138,14 @@ bool Ship::get_orientation() {
 
 ll Ship::get_size() {
 	return size;
+}
+
+ll Ship::get_x() {
+	return x;
+}
+
+ll Ship::get_y() {
+	return y;
 }
 
 Field::Field(Color new_border_color) {
@@ -191,15 +205,20 @@ void Field::show_field(ll x, ll y) {
 			if (field[i][j] == 1) {
 				set_color(LightGray, LightGray);
 			}
-			if (field[i][j] >= 10) {
+			if (field[i][j] == 2) {
 				set_color(Red, Red);
+			}
+			if (field[i][j] >= 10) {
+				set_color(White, White);
 			}
 			cout << block;
 		}
 	}
 }
 
-bool Field::check_pos(ll x, ll y, Ship ship) {
+bool Field::check_pos(Ship ship) {
+	ll x = ship.get_x();
+	ll y = ship.get_y();
 	ll size = ship.get_size();
 	bool flag = 1;
 	bool orientation = ship.get_orientation();
@@ -313,7 +332,9 @@ bool Field::check_pos(ll x, ll y, Ship ship) {
 	return flag;
 }
 
-void Field::set_ship(ll x, ll y, Ship ship) {
+void Field::set_ship(Ship ship) {
+	ll x = ship.get_x();
+	ll y = ship.get_y();
 	ll num = 10 + ships_cnt;
 	++ships_cnt;
 	ships.pb(ship);
@@ -331,13 +352,11 @@ void Field::set_ship(ll x, ll y, Ship ship) {
 void Field::generate_field() {
 	for (int i = 0; i < 4; ++i) {
 		for (int j = i; j < 4; ++j) {
-			Ship ship(4 - i, bool(rand() % 2));
 			bool flag = 1;
 			while (flag) {
-				ll x = rand() % 10;
-				ll y = rand() % 10;
-				if (check_pos(x, y, ship)) {
-					set_ship(x, y, ship);
+				Ship ship(4 - i, rand() % 10, rand() % 10, bool(rand() % 2));
+				if (check_pos(ship)) {
+					set_ship(ship);
 					flag = 0;
 				}
 			}
@@ -350,9 +369,130 @@ void Field::show(ll x, ll y) {
 	show_field(x + 2, y + 1);
 }
 
-void Field::update_field(ll x, ll y) {
+void Field::set_border(Ship ship) {
+	ll x = ship.get_x();
+	ll y = ship.get_y();
+	ll size = ship.get_size();
+	bool orientation = ship.get_orientation();
+	if (orientation) {
+		if (y > 0) {
+			if (field[x][y - 1] == 0) {
+				field[x][y - 1] = 1;
+			}
+			if (x > 0) {
+				if (field[x - 1][y - 1] == 0) {
+					field[x - 1][y - 1] = 1;
+				}
+			}
+			if (x < 9) {
+				if (field[x + 1][y - 1] == 0) {
+					field[x + 1][y - 1] = 1;
+				}
+			}
+		}
+		if (y + size - 1 < 9) {
+			if (field[x][y + size] == 0) {
+				field[x][y + size] = 1;
+			}
+			if (x > 0) {
+				if (field[x - 1][y + size] == 0) {
+					field[x - 1][y + size] = 1;
+				}
+			}
+			if (x < 9) {
+				if (field[x + 1][y + size] == 0) {
+					field[x + 1][y + size] = 1;
+				}
+			}
+		}
+	}
+	else {
+		if (x > 0) {
+			if (field[x - 1][y] == 0) {
+				field[x - 1][y] = 1;
+			}
+			if (y > 0) {
+				if (field[x - 1][y - 1] == 0) {
+					field[x - 1][y - 1] = 1;
+				}
+			}
+			if (y < 9) {
+				if (field[x - 1][y + 1] == 0) {
+					field[x - 1][y + 1] = 1;
+				}
+			}
+		}
+		if (x + size - 1 < 9) {
+			if (field[x + size][y] == 0) {
+				field[x + size][y] = 1;
+			}
+			if (y > 0) {
+				if (field[x + size][y - 1] == 0) {
+					field[x + size][y - 1] = 1;
+				}
+			}
+			if (y < 9) {
+				if (field[x + size][y + 1] == 0) {
+					field[x + size][y + 1] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < size; ++i) {
+		if (orientation) {
+			if (field[x][y + i] == 0) {
+				field[x][y + i] = 1;
+			}
+			if (x > 0) {
+				if (field[x - 1][y + i] == 0) {
+					field[x - 1][y + i] = 1;
+				}
+			}
+			if (x < 9) {
+				if (field[x + 1][y + i] == 0) {
+					field[x + 1][y + i] = 1;
+				}
+			}
+		}
+		else {
+			if (field[x + i][y] == 0) {
+				field[x + i][y] = 1;
+			}
+			if (y > 0) {
+				if (field[x + i][y - 1] != 0) {
+					field[x + i][y - 1] = 1;
+				}
+			}
+			if (y < 9) {
+				if (field[x + i][y + 1] == 0) {
+					field[x + i][y + 1] = 1;
+				}
+			}
+		}
+	}
+}
+
+bool Field::check_pos(ll x, ll y) {
+	return ((field[x][y] == 0) || (field[x][y] > 9));
+}
+
+ll Field::update_field(ll x, ll y) {
 	if (field[x][y] == 0) {
 		field[x][y] = 1;
+		return 0;
+	}
+	if (field[x][y] >= 10) {
+		ships[field[x][y] - 10].hit();
+		if (!ships[field[x][y] - 10].get_is_alive()) {
+			--ships_cnt;
+			set_border(ships[field[x][y] - 10]);
+		}
+		field[x][y] = 2;
+		return 1;
+	}
+	else {
+		return 2;
 	}
 }
 
@@ -368,9 +508,18 @@ int main() {
 	system("cls");
 	field.generate_field();
 	field.show(0, 0);
-
-	set_cursor(0, 13);
-	cout << block;
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			if (field.check_pos(i, j)) {
+				Sleep(150);
+				field.update_field(i, j);
+				set_color(Black, Black);
+				system("cls");
+				field.show(0, 0);
+				cout << "";
+			}
+		}
+	}
 
 	set_color(Black, Black);
 	system("cls");
